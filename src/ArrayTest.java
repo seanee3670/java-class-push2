33c4a5432320aa1a8fa0b9ed0438aa949ea1c26d
seanee3670 <cseok3670@gmail.com>
2022-10-04 13:40:13 +0900

    [update] ArrayTest, CurrencyCnt

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class ArrayTest {
+import java.util.Arrays;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+
+        for (int i = 1; i <= 10; i++) {
+            arr[i - 1] = i;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
 }
---
+++ src/CurrencyCnt.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CurrencyCnt {
+    public static void main(String[] args) throws IOException {
+        /*
+         * 다음과 같이 화폐 매수를 구하는 프로그램을 구현하시오.
+         * ```
+            금액을 넣으세요:[숫자입력]
+            50000원?개
+            10000원?개
+            5000원?개
+            1000원?개
+            500원?개
+            100원?개
+            50원?개
+            10원?개
+            ```
+         */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] amountUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+        int totalAmount;
+
+        System.out.print("금액을 넣으세요:");
+        totalAmount = Integer.valueOf(br.readLine());
+
+        for (int unit : amountUnit) {
+            int quotient = totalAmount / unit;
+            System.out.printf("%d원%d개%n", unit, quotient);
+            totalAmount -= unit * quotient;
+        }
+    }
+
+}
