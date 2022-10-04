6b8f55577e772034a70b022a0564a65f910fb463
seanee3670 <cseok3670@gmail.com>
2022-10-04 14:33:56 +0900

    [update] CurrencyCnt
    comment: 25,000원을 각 화폐 단위로 나누었을 때 몫과 나머지를 출력하는 코드

--- src/CurrencyCnt.java
+++ src/CurrencyCnt.java
@@ -5,34 +5,28 @@ import java.io.InputStreamReader;
 
 public class CurrencyCnt {
 
-    public static void main(String[] args) throws IOException {
-        /*
-         * 다음과 같이 화폐 매수를 구하는 프로그램을 구현하시오.
-         * ```
-            금액을 넣으세요:[숫자입력]
-            50000원?개
-            10000원?개
-            5000원?개
-            1000원?개
-            500원?개
-            100원?개
-            50원?개
-            10원?개
-            ```
-         */
+    public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int curr50000 = 50000;
+        final int curr10000 = 10000;
+        final int curr5000 = 5000;
+        final int curr1000 = 1000;
+        final int curr500 = 500;
+        final int curr100 = 100;
 
-        int[] amountUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
-        int totalAmount;
+        final int[] currencies = { curr50000, curr10000, curr5000, curr1000, curr500, curr100 };
 
-        System.out.print("금액을 넣으세요:");
-        totalAmount = Integer.valueOf(br.readLine());
+        int totalAmount = 25000;
 
-        for (int unit : amountUnit) {
+
+
+        for (int unit : currencies) {
             int quotient = totalAmount / unit;
-            System.out.printf("%d원%d개%n", unit, quotient);
-            totalAmount -= unit * quotient;
+//            System.out.printf("%d원 %d개%n", unit, quotient);
+//            totalAmount -= unit * quotient;
+
+            System.out.printf("%d원 %d개, 나머지: %d %n", unit, quotient, totalAmount - (unit * quotient));
+
         }
     }
 
